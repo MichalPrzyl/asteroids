96bb00d46d579d9f0739acdd3f9ed9690597b61c
Michal Przylucki <michal.przyl@gmail.com>
2024-08-17 00:36:53 +0200

    Collisions with meteors, game over, reseting game

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,1 +1,1 @@
-gcc -o game main.c circle.c -lm -lSDL2 && ./game
+gcc -o game main.c circle.c -lm -lSDL2 -lSDL2_ttf && ./game
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_render.h>
+#include <SDL2/SDL_ttf.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "circle.h"
@@ -8,6 +9,7 @@
 #define GAME_WINDOW_WIDTH 640
 #define GAME_WINDOW_HEIGHT 480
 
+#define SHIP_SIZE 30
 #define MAX_BULLETS 30
 #define BULLET_SIZE 4
 
@@ -40,6 +42,38 @@ struct Meteor {
     struct Point velocity;
     float size;
 };
+
+void reset_game(int *game_over, 
+                SDL_Renderer *renderer,
+                struct Meteor meteors[],
+                struct State *state){
+  *game_over = 0;
+  init_meteors(renderer, meteors);
+  struct Point zero_velocity = { .x = 0, .y = 0 };
+  state->ship.velocity = zero_velocity;
+}
+
+float distance_between_points(struct Point p1, struct Point p2) {
+    float dx = p2.x - p1.x;
+    float dy = p2.y - p1.y;
+    return sqrt(dx * dx + dy * dy);
+}
+
+void check_player_collision(struct Ship *ship, struct Meteor meteors[], int *game_over){
+  for (int i = 0; i < MAX_METEORS; i++){
+    if (check_player_collisions_with_meteor(ship, meteors[i])){
+      printf("GAME OVER\n");
+      *game_over = 1;
+    }
+  }
+}
+
+int check_player_collisions_with_meteor(struct Ship *ship, struct Meteor *meteor){
+  float distance = distance_between_points(ship->position, meteor->position);
+  float ship_radius = SHIP_SIZE / 2;
+  float meteor_radius = meteor->size / 2;
+  return (int)(distance <= (ship_radius + meteor_radius));
+}
 
 void update_meteors_positions(struct Meteor meteors[], float delta_time){
     // Update active bullets positions.
@@ -99,38 +133,37 @@ void draw_bullets(SDL_Renderer *renderer, struct Bullet bullets[]){
 }
 
 void draw_ship(SDL_Renderer *renderer, struct Ship *ship, Uint32 current_time) {
-  int ship_size = 30;
 
   struct Point p1 = {
-      ship->position.x + ship_size * cos(ship->rotation * PI / 180),
-      ship->position.y + ship_size * sin(ship->rotation * PI / 180)
+      ship->position.x + SHIP_SIZE * cos(ship->rotation * PI / 180),
+      ship->position.y + SHIP_SIZE * sin(ship->rotation * PI / 180)
   };
 
   struct Point p2 = {
-      ship->position.x + (ship_size * 0.6) * cos((ship->rotation + 135) * PI / 180),
-      ship->position.y + (ship_size * 0.6) * sin((ship->rotation + 135) * PI / 180)
+      ship->position.x + (SHIP_SIZE * 0.6) * cos((ship->rotation + 135) * PI / 180),
+      ship->position.y + (SHIP_SIZE * 0.6) * sin((ship->rotation + 135) * PI / 180)
   };
 
   struct Point p3 = {
-      ship->position.x + (ship_size * 0.6) * cos((ship->rotation + 225) * PI / 180),
-      ship->position.y + (ship_size * 0.6) * sin((ship->rotation + 225) * PI / 180)
+      ship->position.x + (SHIP_SIZE * 0.6) * cos((ship->rotation + 225) * PI / 180),
+      ship->position.y + (SHIP_SIZE * 0.6) * sin((ship->rotation + 225) * PI / 180)
   };
 
   if (ship->thrusting && ((current_time / 20) % 2 == 0)) {
 
         struct Point flame1 = {
-            ship->position.x + (ship_size * 0.8) * cos((ship->rotation + 180) * PI / 180),
-            ship->position.y + (ship_size * 0.8) * sin((ship->rotation + 180) * PI / 180)
+            ship->position.x + (SHIP_SIZE * 0.8) * cos((ship->rotation + 180) * PI / 180),
+            ship->position.y + (SHIP_SIZE * 0.8) * sin((ship->rotation + 180) * PI / 180)
         };
 
         struct Point flame2 = {
-            ship->position.x + (ship_size * 0.5) * cos((ship->rotation + 160) * PI / 180),
-            ship->position.y + (ship_size * 0.5) * sin((ship->rotation + 160) * PI / 180)
+            ship->position.x + (SHIP_SIZE * 0.5) * cos((ship->rotation + 160) * PI / 180),
+            ship->position.y + (SHIP_SIZE * 0.5) * sin((ship->rotation + 160) * PI / 180)
         };
 
         struct Point flame3 = {
-            ship->position.x + (ship_size * 0.5) * cos((ship->rotation + 200) * PI / 180),
-            ship->position.y + (ship_size * 0.5) * sin((ship->rotation + 200) * PI / 180)
+            ship->position.x + (SHIP_SIZE * 0.5) * cos((ship->rotation + 200) * PI / 180),
+            ship->position.y + (SHIP_SIZE * 0.5) * sin((ship->rotation + 200) * PI / 180)
         };
 
         SDL_SetRenderDrawColor(renderer, 255, 165, 0, 255);  // Pomarańczowy kolor dla płomieni
@@ -152,6 +185,21 @@ int main(int argc, char *argv[]) {
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     return 1;
   }
+  if (TTF_Init() == -1) {
+      printf("TTF_Init: %s\n", TTF_GetError());
+      return 1;
+  }
+
+  // Font initialization
+  TTF_Font *font_large = TTF_OpenFont("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 48);
+  TTF_Font *font_small = TTF_OpenFont("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 24);
+
+  if (!font_large || !font_small) {
+      printf("TTF_OpenFont: %s\n", TTF_GetError());
+      return 1;
+  }
+  
+  SDL_Color white = {255, 255, 255, 255};  // white color
 
   // Window creation.
   SDL_Window *window = SDL_CreateWindow(
@@ -184,6 +232,7 @@ int main(int argc, char *argv[]) {
   float velocity_change_speed_multiplier = 12;
   float rotation_speed_multiplier = 300;
   float bullet_speed = 300;
+  int game_over = 0;
 
   // Bullet list
   struct Bullet bullets[MAX_BULLETS];
@@ -199,6 +248,10 @@ int main(int argc, char *argv[]) {
 
     while (SDL_PollEvent(&e) != 0) {
       if (e.type == SDL_KEYDOWN) {
+
+        if (e.key.keysym.sym == SDLK_r) {
+          reset_game(&game_over, renderer, meteors, &state);
+        }
 
         // Shooting
         if (e.key.keysym.sym == SDLK_SPACE) {
@@ -227,63 +280,92 @@ int main(int argc, char *argv[]) {
     // Keyboard state
     const Uint8 *keyboard_state = SDL_GetKeyboardState(NULL);
 
-    if (keyboard_state[SDL_SCANCODE_D]) {
-      state.ship.rotation += 1.0f * delta_time * rotation_speed_multiplier;
-    }
-    if (keyboard_state[SDL_SCANCODE_A]) {
-      state.ship.rotation -= 1.0f * delta_time * rotation_speed_multiplier;
-    }
-    if (keyboard_state[SDL_SCANCODE_W]) {
-      float x_vel_change = cos(state.ship.rotation * PI / 180) * delta_time * velocity_change_speed_multiplier;
-      state.ship.velocity.x += x_vel_change;
-
-      float y_vel_change = sin(state.ship.rotation * PI / 180) * delta_time * velocity_change_speed_multiplier;
-      state.ship.velocity.y += y_vel_change;
-      state.ship.thrusting = 1;
-    } else{
-      state.ship.thrusting = 0;
-    }
-
-    // Update active bullets positions.
-    for (int i = 0; i < MAX_BULLETS; i++){
-      if (bullets[i].active){
-        bullets[i].position.x += bullets[i].velocity.x * delta_time;
-        bullets[i].position.y += bullets[i].velocity.y * delta_time;
-      }
-
-      // Check if bullet is outside the window. If so, change it's 'active' field to 0.
-      if (bullets[i].position.x < 0 || bullets[i].position.x > GAME_WINDOW_WIDTH ||
-            bullets[i].position.y < 0 || bullets[i].position.y > GAME_WINDOW_HEIGHT) {
-            bullets[i].active = 0;
-        }
-    }
-    update_meteors_positions(meteors, delta_time);
-    
-    // Update player's ship position.
-    state.ship.position.x += state.ship.velocity.x * delta_time * speed_multiplier;
-    state.ship.position.y += state.ship.velocity.y * delta_time * speed_multiplier;
-    // Going beyond window width/height
-    // X axis
-    if (state.ship.position.x > GAME_WINDOW_WIDTH) {
-      state.ship.position.x = 0;
-    } else if (state.ship.position.x < 0) {
-      state.ship.position.x = GAME_WINDOW_WIDTH;
-    }
-    // Y axis
-    if (state.ship.position.y > GAME_WINDOW_HEIGHT) {
-      state.ship.position.y = 0;
-    } else if (state.ship.position.y < 0) {
-      state.ship.position.y = GAME_WINDOW_HEIGHT;
-    }
-
-    /* printf("state.ship.position.x: %f\n", state.ship.position.x); */
-    // Renderer stuff
-    // Clear renderer
     SDL_RenderClear(renderer);
 
-    draw_ship(renderer, &state.ship, currentTime);
-    draw_bullets(renderer, bullets);
-    draw_meteors(renderer, meteors);
+    if (!game_over){
+      if (keyboard_state[SDL_SCANCODE_D]) {
+        state.ship.rotation += 1.0f * delta_time * rotation_speed_multiplier;
+      }
+      if (keyboard_state[SDL_SCANCODE_A]) {
+        state.ship.rotation -= 1.0f * delta_time * rotation_speed_multiplier;
+      }
+      if (keyboard_state[SDL_SCANCODE_W]) {
+        float x_vel_change = cos(state.ship.rotation * PI / 180) * delta_time * velocity_change_speed_multiplier;
+        state.ship.velocity.x += x_vel_change;
+
+        float y_vel_change = sin(state.ship.rotation * PI / 180) * delta_time * velocity_change_speed_multiplier;
+        state.ship.velocity.y += y_vel_change;
+        state.ship.thrusting = 1;
+      } else{
+        state.ship.thrusting = 0;
+      }
+
+      // Update active bullets positions.
+      for (int i = 0; i < MAX_BULLETS; i++){
+        if (bullets[i].active){
+          bullets[i].position.x += bullets[i].velocity.x * delta_time;
+          bullets[i].position.y += bullets[i].velocity.y * delta_time;
+        }
+
+        // Check if bullet is outside the window. If so, change it's 'active' field to 0.
+        if (bullets[i].position.x < 0 || bullets[i].position.x > GAME_WINDOW_WIDTH ||
+              bullets[i].position.y < 0 || bullets[i].position.y > GAME_WINDOW_HEIGHT) {
+              bullets[i].active = 0;
+          }
+      }
+      update_meteors_positions(meteors, delta_time);
+      
+      // Update player's ship position.
+      state.ship.position.x += state.ship.velocity.x * delta_time * speed_multiplier;
+      state.ship.position.y += state.ship.velocity.y * delta_time * speed_multiplier;
+      // Going beyond window width/height
+      // X axis
+      if (state.ship.position.x > GAME_WINDOW_WIDTH) {
+        state.ship.position.x = 0;
+      } else if (state.ship.position.x < 0) {
+        state.ship.position.x = GAME_WINDOW_WIDTH;
+      }
+      // Y axis
+      if (state.ship.position.y > GAME_WINDOW_HEIGHT) {
+        state.ship.position.y = 0;
+      } else if (state.ship.position.y < 0) {
+        state.ship.position.y = GAME_WINDOW_HEIGHT;
+      }
+      /* check_player_collisions() */
+      check_player_collision(&state.ship, meteors, &game_over);
+
+      // Renderer stuff
+      // Clear renderer
+
+      draw_ship(renderer, &state.ship, currentTime);
+      draw_bullets(renderer, bullets);
+      draw_meteors(renderer, meteors);
+
+    }else{
+      // When GAME OVER
+      SDL_Surface *surface_message = TTF_RenderText_Solid(font_large, "Game Over", white);
+      SDL_Texture *message_texture = SDL_CreateTextureFromSurface(renderer, surface_message);
+
+      SDL_Surface *surface_instruction = TTF_RenderText_Solid(font_small, "Press R to restart", white);
+      SDL_Texture *instruction_texture = SDL_CreateTextureFromSurface(renderer, surface_instruction);
+
+      // Ustalanie pozycji tekstu "Game Over" na środku ekranu
+      SDL_Rect message_rect;  // Współrzędne dla tekstu "Game Over"
+      message_rect.x = (GAME_WINDOW_WIDTH - surface_message->w) / 2;
+      message_rect.y = (GAME_WINDOW_HEIGHT - surface_message->h) / 2 - 50;
+      message_rect.w = surface_message->w;
+      message_rect.h = surface_message->h;
+
+      // Ustalanie pozycji tekstu "Press R to restart" poniżej "Game Over"
+      SDL_Rect instruction_rect;  // Współrzędne dla tekstu "Press R to restart"
+      instruction_rect.x = (GAME_WINDOW_WIDTH - surface_instruction->w) / 2;
+      instruction_rect.y = message_rect.y + message_rect.h + 20;  // 20 pikseli poniżej "Game Over"
+      instruction_rect.w = surface_instruction->w;
+      instruction_rect.h = surface_instruction->h;
+
+      SDL_RenderCopy(renderer, message_texture, NULL, &message_rect);
+      SDL_RenderCopy(renderer, instruction_texture, NULL, &instruction_rect);
+    }
 
     // black color background
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
